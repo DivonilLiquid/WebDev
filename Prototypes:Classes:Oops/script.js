97fa5e90b7325788a7288fe4dd29066eebeb6daa
DivonilLiquid <mait.madhur@gmail.com>
2021-05-20 21:33:19 +0530

    Prototypes/Classes/Oops

---
+++ Prototypes:Classes:Oops/script.js
@@ -0,0 +1,7 @@
+/*
+Prototype is an object which help us to excess property of object,array,variable and others
+*/
+/*
+JS engine automatically assigns a prototype to any object or element so that we can access it's 
+property using that prototype 
+*/
